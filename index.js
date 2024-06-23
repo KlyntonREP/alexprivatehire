77ad81a657b34f0e9beaeb33203a7aa0300eafc8
Ahiwe-Onuoha Clinton <clinkesh57@gmail.com>
2024-06-23 09:34:12 +0100

    hope this works

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, 'public')));
-// port = process.env.PORT || 4564;
+port = process.env.PORT || 4564;
 
 app.use("", routes);
 
-// app.listen(port)
-// console.log("Server Running On Port: " + port)
-export default app;
+app.listen(port)
+console.log("Server Running On Port: " + port)
