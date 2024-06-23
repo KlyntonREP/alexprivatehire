57cd657ccc2d20fd9f560d6037f65eeaa2f0f1c4
Ahiwe-Onuoha Clinton <clinkesh57@gmail.com>
2024-06-23 09:55:55 +0100

    i really hope this works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, 'public')));
 port = process.env.PORT || 4564;
