41e260ce033367e38ba25ee8f6f3c6072d6cdc8d
Ahiwe-Onuoha Clinton <clinkesh57@gmail.com>
2024-07-03 07:44:26 +0100

    this will worK

--- router/router.js
+++ router/router.js
@@ -4,9 +4,17 @@ const controller = require('../controller/controller')
 router = express.Router();
 
 router.get("/", controller.index);
-router.get('/sitemap.xml', function(req, res) {
-    res.set('Content-Type', 'text/xml')
-    res.sendFile(path.resolve('sitemap.xml'));
+router.get('/sitemap.xml', function (req, res, next) {
+    res.type('text/xml')
+    res.send(
+        `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+        <url>
+        <loc>https://alexprivatehire.com/</loc>
+        <lastmod>2024-07-02T05:47:57+00:00</lastmod>
+        </url>
+        </urlset>`);
 });
 router.post("/sendMail", controller.submit)
 
