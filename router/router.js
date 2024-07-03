77e4ef8390e183548465304eac1a2b8cccb1f2ad
Ahiwe-Onuoha Clinton <clinkesh57@gmail.com>
2024-07-03 09:10:36 +0100

    fix robot

--- router/router.js
+++ router/router.js
@@ -20,9 +20,8 @@ router.get('/robots.txt', function (req, res, next) {
     res.type('text/xml')
     res.send(
         `User-agent: *
-    Disallow: /private/
-
-    Sitemap: https://alexprivatehire.com/sitemap.xml`);
+        Disallow: /private/
+        Sitemap: https://alexprivatehire.com/sitemap.xml`);
 });
 router.post("/sendMail", controller.submit)
 
